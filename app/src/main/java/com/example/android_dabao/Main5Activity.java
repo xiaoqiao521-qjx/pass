60942558a4882d4d90ce5e790b459bd0a79fdba0
“qiaojianxun” <qiaojianxun@aliyun.com>
2020-05-04 16:19:08 +0800

    解决代码冲突

--- app/src/main/java/com/example/android_dabao/Main5Activity.java
+++ app/src/main/java/com/example/android_dabao/Main5Activity.java
@@ -22,11 +22,11 @@ public class Main5Activity extends AppCompatActivity {
 
     private void initView() {
         mM5Tv = (TextView) findViewById(R.id.m5_tv);
-        mM5Tv.setText("我在你在哪");
+        mM5Tv.setText("我不管你在哪例");
         mM5Tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mM5Tv.setText("啊实打实的先吃饭");
+                mM5Tv.setText("阿萨的协方差");
             }
         });
 
