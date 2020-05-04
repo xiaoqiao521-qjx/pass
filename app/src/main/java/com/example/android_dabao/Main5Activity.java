a3b1519164c0b2b606b620c3a20ac2c167135b20
“qiaojianxun” <qiaojianxun@aliyun.com>
2020-05-04 15:49:45 +0800

    M5中添加了text

--- app/src/main/java/com/example/android_dabao/Main5Activity.java
+++ app/src/main/java/com/example/android_dabao/Main5Activity.java
@@ -1,14 +1,25 @@
 package com.example.android_dabao;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Main5Activity extends AppCompatActivity {
+
+    /**
+     * m5
+     */
+    private TextView mM5Tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        initView();
+    }
+
+    private void initView() {
+        mM5Tv = (TextView) findViewById(R.id.m5_tv);
     }
 }
