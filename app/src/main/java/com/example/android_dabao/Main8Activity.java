a70358e9798df93aec22794a9b33f91cc0d9dac4
“qiaojianxun” <qiaojianxun@aliyun.com>
2020-05-04 17:49:16 +0800

    修改仓库中M8的信息

--- app/src/main/java/com/example/android_dabao/Main8Activity.java
+++ app/src/main/java/com/example/android_dabao/Main8Activity.java
@@ -1,14 +1,23 @@
 package com.example.android_dabao;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Main8Activity extends AppCompatActivity {
+
+    private TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
+        initView();
+    }
+
+    private void initView() {
+        mTv = (TextView) findViewById(R.id.tv);
+        mTv.setText("爱上对方根本");
     }
 }
