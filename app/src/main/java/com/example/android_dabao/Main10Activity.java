7f6623929e974e638d93bfcb614821899cd39e8b
“qiaojianxun” <qiaojianxun@aliyun.com>
2020-05-04 23:48:33 +0800

    m100000冲突

--- app/src/main/java/com/example/android_dabao/Main10Activity.java
+++ app/src/main/java/com/example/android_dabao/Main10Activity.java
@@ -1,14 +1,23 @@
 package com.example.android_dabao;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Main10Activity extends AppCompatActivity {
+
+    private TextView mTv10;
+    private TextView mTv100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main10);
+        initView();
+    }
+
+    private void initView() {
+
     }
 }
