f264d2951b98614ccd6df4f9c2be7e2a89a0328e
“qiaojianxun” <qiaojianxun@aliyun.com>
2020-05-04 15:08:01 +0800

    提交修改信息

---
+++ app/src/main/java/com/example/android_dabao/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android_dabao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * 跳Fragment1
+     */
+    private Button mBtn1;
+    private Fragment1 fragment;
+    /**
+     * Main2
+     */
+    private Button mBtn5;
+    private TextView mTvText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBtn1 = (Button) findViewById(R.id.btn1);
+        mBtn1.setOnClickListener(this);
+        mBtn5 = (Button) findViewById(R.id.btn5);
+        mTvText = (TextView) findViewById(R.id.tv_text);
+        mBtn5.setOnClickListener(this);
+        mTvText.setText("为宣传");
+  
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.btn1:
+                Intent intent = new Intent(this, Main2Activity.class);
+                intent.putExtra("type", "MainActivity");
+                startActivity(intent);
+                break;
+            case R.id.btn5:
+                Intent intent2 = new Intent(this, Main2Activity.class);
+                startActivity(intent2);
+                break;
+        }
+    }
+}
