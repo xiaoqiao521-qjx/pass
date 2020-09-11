e80d3164304d1f360bccaf1879e1ae077e9ad15b
qiaojianxun <qiaojianxun@aliyun.com>
2020-09-11 09:03:32 +0800

    我的能提交么？ 我的能提交么？

--- app/src/main/java/com/example/android_dabao/MainActivity.java
+++ app/src/main/java/com/example/android_dabao/MainActivity.java
@@ -58,15 +58,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 break;
             case R.id.btn2:
 
+                ///devpp
 
 
+                //我的啊是这东西非常
                 parameter = camera.getParameters();
-
-
-
-
                 parameter.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
                 camera.setParameters(parameter);
+
                 break;
         }
     }
